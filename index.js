85ef3785865626e6ffc9db4a63e169d74ad77a46
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-05-23 02:37:19 -0600

    adding and testing event listeners, don't forget to call the method that adds the listener... not my day.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const mbutton = document.getElementById("button");
+    mbutton.addEventListener('click', function() {
+        console.log("button was clicked!");
+        alert("I was clicked!");
+    });
 }
+addingEventListener();
